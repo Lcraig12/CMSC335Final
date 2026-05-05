f4d29237b702f0c3de59485a11c611b20939a23d
Yaniv Ashwal <yanivashwal28@gmail.com>
2026-05-05 18:52:12 -0400

    added app.listen()

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
-process.stdin.setEncoding("utf8");
-
 const portNum = 5001;
 
 const app = express();
@@ -12,8 +10,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
 
-console.log(`Web server started and running at http://localhost:${portNum}\n`);
+app.listen(portNum, () => { 
+    console.log(`Web server started and running at http://localhost:${portNum}\n`);
+});
 
 app.get("/", (req, res) => {
     res.render("index");
-})
+});

7f7104e477a6b116a93093a3032c037ef44d0a8a
Yaniv Ashwal <yanivashwal28@gmail.com>
2026-05-05 18:41:55 -0400

    main.js mostly

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+
+process.stdin.setEncoding("utf8");
+
+const portNum = 5001;
+
+const app = express();
+ 
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
+
+console.log(`Web server started and running at http://localhost:${portNum}\n`);
+
+
+app.get("/", (req, res) => {
+    res.render("homeScreen.ejs")
+})

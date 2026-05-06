ca19d8085af6077c8e3b7b21b8886270888d147f
Parborway <146022198+Parborway@users.noreply.github.com>
2026-05-06 09:13:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -27,6 +27,30 @@ app.post("/play", (req,res) => {
     res.render("open",variables);
 });
 
+app.post("/home", (req,res) => { //this is specifically for when the game gets saved.
+    let {world,worldname,username} = req.body;
+    //this is where database stuff has to happen.
+    let gs = /*some code that gets the worlds vector from the database given the username IMPLEMENT THIS*/
+    let gso = JSON.parse(gs);
+    let findex = -1;
+    for (let i = 0; i < gso.length; i++) {
+		if(gso[i].name === worldname) {
+		    findex = i;
+		}
+    }
+    if (findex == -1) {
+		console.error(`Error: Could not find world ${worldname} in files of user ${username}`);
+    } else {
+		gso[findex].world = world;//this is the part of the code that actually saves it
+    }
+    let g2 = JSON.stringify(gso);
+	
+    /*now we Need some code that will put g2 back in the database where gs was previously IMPLEMENT THIS TOO*/
+
+    res.render("homeScreen");//I'm not sure if going directly to home with no fields is correct but that's a problem for later.
+});
+
+
 //this function takes the raw worlds from the table, the username, and the path, and returns a string containing the table that is ready to be
 //inserted into an innerHTML element somewhere. -Ian
 function getTableFromWorlds(worlds,username,path) {//this messed up formatting is what I get for writing this in notepad.
